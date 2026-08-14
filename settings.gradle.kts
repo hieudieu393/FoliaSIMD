76221c1bffa4736133f531148e229cdc94e2c3a3
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-14 07:43:47 +0700

    Add SIMD support for 26.1.x

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "folia"
+rootProject.name = "FoliaSIMD"
 
 include("folia-api")
 include("folia-server")
